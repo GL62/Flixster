97f8f4f01e6c49705304364ba838d9459c1e5a4a
GuyL <lloydlingani@gmail.com>
2020-10-05 00:46:47 -0400

    Week 2 Codepath Flicks update

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -24,7 +24,7 @@ import okhttp3.Headers;
 public class DetailActivity extends YouTubeBaseActivity {
 
     private static final String YOUTUBE_API_KEY = "AIzaSyAKS0mLH3fQxeV0Bmss73cC_6Pd1hvpACo";
-    public static final String VIDEOS_URL = "https:https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
 
     TextView tvTitle;
